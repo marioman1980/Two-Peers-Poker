c13030ba47f821b31485a069dbd669774a7d1782
marioman1980 <j.kamradcliffe1980@gmail.com>
2016-11-21 19:31:50 +0000

    Show host ID
    Call hostView displayID method.
    Provide alert message to user

--- js/controllers/hostController.js
+++ js/controllers/hostController.js
@@ -4,12 +4,9 @@ define(['jquery', 'models/hostModel', 'views/hostView', 'functions'], function($
   function start(){
     $('#btn-host-game').click(function(){
       functions.gameFunctions.prototype.loadTable('#user-name', '#host-content', '#host-name');
-      //hostView.render.prototype.displayId(myId);
-      
-    }); 
-
-
-  
+      hostView.render.prototype.displayId(myId);  
+      alert("Give your ID to a friend so they can \"Join\" your game");
+    });  
   }
   
 
