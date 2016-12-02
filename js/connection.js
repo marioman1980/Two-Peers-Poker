75c68b81ba41c120f69d42ad31e71dc2e51c294f
James Kam-Radcliffe <j.kamradcliffe1980@gmail.com>
2016-12-02 21:10:30 +0000

    Move selectCard() to Model

--- js/connection.js
+++ js/connection.js
@@ -6,6 +6,8 @@ define(['peerjs', 'connect', 'models/model'], function(peerjs, connect, model){
     var name = null;
     var conn = null;  
     var connMade = false;
+    
+    var dealtCard;
  /* New peer connection with our heroku server */
     peer = new Peer({　
         host: 'twopeers.herokuapp.com',
@@ -18,7 +20,7 @@ define(['peerjs', 'connect', 'models/model'], function(peerjs, connect, model){
       myId = id;
       console.log('My peer ID is: ' + myId); /* Console message confirming peer & ID created */   
     });					
-var testCard;
+
   /* Host peer ID used to establish connection and guest name sent to host peer */     
     this.joinGame = function(){  
       name = $('#guest-name').val();
@@ -31,8 +33,7 @@ var testCard;
     }
 
     var a = 1;
-    peer.on('connection', function(connection){
-      
+    peer.on('connection', function(connection){     
       conn = connection;
       host_id = connection.peer;
     //Whilst connection is open, any data sent will be handled by 'handleMessage' function
@@ -48,17 +49,11 @@ var testCard;
         $('#display-id').addClass('remove');
         
         connMade = true;
-
-      }  
-
-      
+      }        
     }); 
-
-
-    
+  
     function handleCards(data){
       $(data.element).append(data.img);
-      //alert(data.crd);
     }
 
   //Sending messages	
@@ -69,78 +64,63 @@ var testCard;
 
 
   
-var cardRanks = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
-var cardSuits = ['C', 'D', 'H', 'S'];
-var value,
-    suit,
-		rank,
-		card;    
-    
-function selectCard(){
-	value = Math.floor(Math.random() * cardRanks.length);//Random value
-	suit = Math.floor(Math.random() * 4);//Random suit value
-	for (i=0;i<13;i++){
-		if (value == i){
-			rank = cardRanks[i];//Get rank of card
-		}
-	}
-  /*Give card a value if it's a face card*/
-	if (rank == 'J') value = 11;
-	else if (rank == 'Q') value = 12;
-	else if (rank == 'K') value = 13;
-	else if (rank == 'A') value = 14;
-	else value = value + 1;	
-	for (i=0;i<4;i++){
-		if (suit == i){
-			suit = cardSuits[i];//Get suit
-		}
-	}	
-	card = rank + suit;	//Create card from rank & suit
-  image = '<img src="../Two-Peers-Poker/images/allCards/' + card + '.jpg">'
-  console.log(card);
-
-}    
+//var cardRanks = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+//var cardSuits = ['C', 'D', 'H', 'S'];
+//var value,
+//    suit,
+//		rank,
+//		card;    
+//    
+//function selectCard(){
+//	value = Math.floor(Math.random() * cardRanks.length);//Random value
+//	suit = Math.floor(Math.random() * 4);//Random suit value
+//	for (i=0;i<13;i++){
+//		if (value == i){
+//			rank = cardRanks[i];//Get rank of card
+//		}
+//	}
+//  /*Give card a value if it's a face card*/
+//	if (rank == 'J') value = 11;
+//	else if (rank == 'Q') value = 12;
+//	else if (rank == 'K') value = 13;
+//	else if (rank == 'A') value = 14;
+//	else value = value + 1;	
+//	for (i=0;i<4;i++){
+//		if (suit == i){
+//			suit = cardSuits[i];//Get suit
+//		}
+//	}	
+//	card = rank + suit;	//Create card from rank & suit
+//  image = '<img src="../Two-Peers-Poker/images/allCards/' + card + '.jpg">'
+//  console.log(card);
+//
+//}    
  
   /*  Deal a card to each player.
       Function takes the corresponding element for each player
       and ouputs the relevant image */
+    
     function dealCard(primaryElement, secondaryElement){
-      selectCard();
-      console.log(value);
+      dealtCard = model.selectCard();
       sendMessage({
         element: primaryElement,
-        img: image
+        img: dealtCard.image
       });
       sendMessage({
         element: secondaryElement,
-        img: image
+        img: dealtCard.image
       });
     }
     
     function dealStartCards(){
     /* Deal two cards to each player */
       dealCard('#host-card', '#guest-host-card');
+      console.log(dealtCard.rank);
       dealCard('#host-card', '#guest-host-card');
       dealCard('#guest-card', '#host-guest-card');
       dealCard('#guest-card', '#host-guest-card');      
       
-//      selectCard();
-//      sendMessage({element: '#host-card', img: image, crd: card});
-//      sendMessage({element: '#guest-host-card', img: image});  
-//      console.log(value);
-//      selectCard();
-//      sendMessage({element: '#host-card', img: image, crd: card});
-//      sendMessage({element: '#guest-host-card', img: image}); 
-//      console.log(value);
-//    /* Deal guest two cards */
-//      selectCard();
-//      sendMessage({element: '#guest-card', img: image, crd: card});
-//      sendMessage({element: '#host-guest-card', img: image}); 
-//      console.log(value);
-//      selectCard();
-//      sendMessage({element: '#guest-card', img: image, crd: card});
-//      sendMessage({element: '#host-guest-card', img: image}); 
-//      console.log(value);
+
     }  
     
  
