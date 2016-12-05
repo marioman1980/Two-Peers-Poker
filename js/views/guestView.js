52e7819a2d3a360af4ee10fa91bbf74ad1ca50a1
James Kam-Radcliffe <j.kamradcliffe1980@gmail.com>
2016-12-05 19:10:10 +0000

    Remove unnecessary files

--- js/views/guestView.js
+++
@@ -1,7 +0,0 @@
-define(['jquery', 'connection', 'models/guestModel', 'views/guestView', 'functions'], function($, connection, guestModel, guestView, functions){
-  
-  $('#btn-join-game').click(function(){
-    connection.joinGame();
-  });
-  
-});
