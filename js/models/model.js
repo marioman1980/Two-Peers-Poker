c49f91699057d72621e449692479a70e22583f68
James Kam-Radcliffe <j.kamradcliffe1980@gmail.com>
2016-12-13 21:52:48 +0000

    Create deck object

--- js/models/model.js
+++ js/models/model.js
@@ -2,8 +2,14 @@ define(['jquery', 'connection', 'functions'], function($, connection, functions)
  
   function model(){
     
-    var cardRanks = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
-    var cardSuits = ['C', 'D', 'H', 'S'];
+    deck = {
+      cardRanks: ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'],
+      cardSuits: ['C', 'D', 'H', 'S'],
+      dealtCards: []
+    }
+    
+//    var cardRanks = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+//    var cardSuits = ['C', 'D', 'H', 'S'];
     var value,
         suit,
         rank,
@@ -12,14 +18,14 @@ define(['jquery', 'connection', 'functions'], function($, connection, functions)
         dealtCard;    
     
     function selectCard(){
-      value = Math.floor(Math.random() * cardRanks.length);//Random value
+      value = Math.floor(Math.random() * deck.cardRanks.length);//Random value
       suit = Math.floor(Math.random() * 4);//Random suit value
       for (i=0;i<13;i++){
         if (value == i){
-          rank = cardRanks[i];//Get rank of card
+          rank = deck.cardRanks[i];//Get rank of card
         }
       }
-      /*Give card a value if it's a face card*/
+/* Give card a value if it's a face card */
       if (rank == 'J') value = 11;
       else if (rank == 'Q') value = 12;
       else if (rank == 'K') value = 13;
@@ -27,13 +33,14 @@ define(['jquery', 'connection', 'functions'], function($, connection, functions)
       else value = value + 1;	
       for (i=0;i<4;i++){
         if (suit == i){
-          suit = cardSuits[i];//Get suit
+          suit = deck.cardSuits[i];//Get suit
         }
       }	
-      card = rank + suit;	//Create card from rank & suit
+/* Create card from rank & suit */      
+      card = rank + suit;	
       image = '<img src="../Two-Peers-Poker/images/allCards/' + card + '.jpg">';
       console.log(card);
-      
+/* Card object to return each time a card is dealt */      
       var dealtCard = {
         value: value,
         suit: suit,
@@ -47,61 +54,57 @@ define(['jquery', 'connection', 'functions'], function($, connection, functions)
     displayImage = function(data){
       eval(data.doStuff);
     }
-//   sendData = function(data){
-//     eval(data.doStuff);
-//   }   
 
-  //Sending messages	
+/* Sending messages */	
     sendMessage = function(data, handleData){
       conn.send(data);
       handleData(data);
     }
    
  
-  /*  Deal a card to each player.
-      Function takes the corresponding element for each player
-      and ouputs the relevant image */   
+/*  
+Deal a card to each player.
+Function takes the corresponding element for each player
+and ouputs the relevant image 
+*/   
     function dealCard(primaryElement, secondaryElement, faceUp){
       dealtCard = selectCard();
       var faceUp = faceUp;
       sendMessage({
         doStuff: "$('" + primaryElement + "').append('" + dealtCard.image +"')"
-//        element: primaryElement,
-//        img: dealtCard.image
       }, displayImage);
       var image;
       if (faceUp == true){image = dealtCard.image;}
       else {image = '<img src="../Two-Peers-Poker/images/allCards/cardBack.jpg">'}
       sendMessage({
         doStuff: "$('" + secondaryElement + "').append('" + image +"')"
-//        element: secondaryElement,
-//        img: image
-        //img: '<img src="../Two-Peers-Poker/images/allCards/cardBack.jpg">'
-        //img: dealtCard.image
       }, displayImage);
     }
     
     this.dealStartCards = function(){
-    /* Deal two cards to each player */
+/* Deal two cards to each player */
       dealCard('#host-card', '#guest-host-card', false);
       console.log(dealtCard.rank);
       dealCard('#host-card', '#guest-host-card', true);
       dealCard('#guest-card', '#host-guest-card', false);
       dealCard('#guest-card', '#host-guest-card', true);      
     } 
-
+/* Player object */
     function Player(playerName, playerType){
       this.name = playerName;
       this.type = playerType;
       this.hand = [];
       this.bank = 0;
       
-    }    
-    
+    }       
     this.createPlayer = function(type){
       player = new Player(localStorage.twoPeersUserName, type);
     }
-    
+
+/* Hand object */
+    function Hand(){
+      
+    }
     
   }/* END */
   
