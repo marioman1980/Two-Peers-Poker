d8be706792b9e84a52c6d3d666c098ab3f1e982f
James Kam-Radcliffe <j.kamradcliffe1980@gmail.com>
2016-12-20 19:36:34 +0000

    Include jQuery UI

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ requirejs.config({
   baseUrl: 'js',
   paths: {
     jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min',
+    jqueryui: 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min',
     peerjs: 'https://cdnjs.cloudflare.com/ajax/libs/peerjs/0.3.8/peer'
   }
 });
