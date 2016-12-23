8d3a3585c3e5e5688f7fe4a1ac2817308b0ffc30
James Kam-Radcliffe <j.kamradcliffe1980@gmail.com>
2016-12-23 10:23:26 +0000

    evaluateHand works using test hand
    Function evaluates all possible hands, logging to console using test
    hand. In the event of matching hands (ie. same pair) no kicker is
    applied

--- js/models/model.js
+++ js/models/model.js
@@ -179,12 +179,17 @@ and ouputs the relevant image
     ===========EVALUATE HANDS===============================*/
     
 evaluateHand = function(){
+  var handName;
+  var handScore = 0;
+  var i;
+  var j;
   
-  hostPlayer.hand.cards[0] = {value: 5, suit: 'S'};
-  hostPlayer.hand.cards[1] = {value: 14, suit: 'S'};
-  hostPlayer.hand.cards[2] = {value: 3, suit: 'S'};
-  hostPlayer.hand.cards[3] = {value: 2, suit: 'S'};
-  hostPlayer.hand.cards[4] = {value: 4, suit: 'S'};
+  // Test hand
+  hostPlayer.hand.cards[0] = {value: 9, suit: 'S'};
+  hostPlayer.hand.cards[1] = {value: 6, suit: 'S'};
+  hostPlayer.hand.cards[2] = {value: 8, suit: 'S'};
+  hostPlayer.hand.cards[3] = {value: 8, suit: 'C'};
+  hostPlayer.hand.cards[4] = {value: 6, suit: 'S'};
 
 /* Sort cards into ascending order */  
   hostPlayer.hand.cards.sort(function(a, b){
@@ -208,20 +213,114 @@ evaluateHand = function(){
   
 /* Check for flush */
   var flush;
-  for (var i=1; i<5; i++){
-    
-    var j = i - 1;
-    if (hostPlayer.hand.cards[i].suit != hostPlayer.hand.cards[j].suit){
+  for (i=1; i<5; i++){
+    if (hostPlayer.hand.cards[i].suit != hostPlayer.hand.cards[0].suit){
       flush = false;
       break
     }
     else{
       flush = true;
-    }
-         
+    }       
   }
-  alert (flush);
-}    
+  console.log('flush = ' + flush);
+  if (flush == true){
+    handScore += 500;
+    handScore += hostPlayer.hand.cards[4].value;
+    handName = 'Flush';
+  }
+  
+  /* Check for straight */
+  var straight;
+  for (i=1; i<5; i++){
+    j = i - 1;
+    if ((hostPlayer.hand.cards[i].value - hostPlayer.hand.cards[j].value) != 1){
+      straight = false;
+      break;
+    }
+    else{
+      straight = true;
+    }
+  }  
+  console.log('straight = ' + straight);
+  if (straight == true){
+    handScore += 400;
+    handScore += hostPlayer.hand.cards[4].value;
+    handName = 'Straight';
+  }
+  
+  
+  
+/* Check for pairs, trips, quads */  
+  if ((flush == false) && (straight == false)){
+    console.log('No straight or flush');
+    
+    var pairValue;
+    var pairsArray = [];
+    /* Check for matching card values */
+    for (i=1; i<5; i++){
+      j = i - 1;
+      if ((hostPlayer.hand.cards[i].value - hostPlayer.hand.cards[j].value) == 0){
+        pairsArray[j] = 1;
+        pairValue = hostPlayer.hand.cards[i].value;
+      }
+      else{
+        pairsArray[j] = 0;
+      }
+    }
+    /* Check total no. of pairs */
+    var pairsTotal = 0;
+    for (i=0; i<4; i++){
+      pairsTotal = pairsTotal + pairsArray[i];
+    }
+    if(pairsTotal == 1){ /* Single pair */
+      handScore += (100 + pairValue);
+      handName = 'Pair';
+    }
+    else if (pairsTotal == 0){ /* High card */
+      handScore += (hostPlayer.hand.cards[4].value);  
+      handName = 'High Card';
+    }
+  /* 
+    Full house - There's effectively 3 pairs due to 3 of kind 
+    Either 2nd & 3rd or 3rd & 4th won't be matched
+  */  
+    else if ((pairsTotal == 3) && ((pairsArray[1] == 0) || (pairsArray[2] == 0))){
+      handScore += (600 + hostPlayer.hand.cards[2].value);
+      handName = 'Full House';
+    }
+  /* 
+    Four of a kind - There's effectively 3 pairs again
+    Either 1st & 2nd or 4th & 5th won't be matched
+  */     
+    else if ((pairsTotal == 3) && ((pairsArray[0] == 0) || (pairsArray[3] == 0))){
+      handScore += (700 + hostPlayer.hand.cards[3].value);
+      handName = 'Quads';      
+    }
+  /* 3-of-a-kind 2 pairs will be made up of 3 cards */  
+    else if (pairsTotal == 2){
+      if (((pairsArray[0] == 1) && (pairsArray[1] == 1)) || ((pairsArray[1] == 1) && (pairsArray[2] == 1)) || ((pairsArray[2] == 1) && (pairsArray[3] == 1))){
+        handScore += (300 + hostPlayer.hand.cards[2].value);
+        handName = 'Trips';          
+      }  
+      else{
+      /* Two Pairs - only remaining combination*/  
+        handScore += (200 + hostPlayer.hand.cards[3].value);
+        handName = 'Two Pairs';  
+      }
+    }
+    
+    
+    
+    
+  }/* End pairs, trips, quads check */
+  
+  
+  
+  console.log(handName);
+  console.log(handScore);   
+}   /* END EVALUATE HAND */ 
+
+
     
     
     
